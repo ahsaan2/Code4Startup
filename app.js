0f0936efcff3805952f1169208f370c203c4dad7
Ahsaan <ahsaan1345@outlook.com>
2024-11-08 20:29:39 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hey nodejs, Ahsaan here')
+// creating a server
+var express = require("express");
+// creating the instance of the application, this app object will be used to define the route and middleware.
+var app = express();
+// defining the route for the get method
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.listen(3000, function() {
+    console.log(`Server is running on http://localhost:3000`);
+});
